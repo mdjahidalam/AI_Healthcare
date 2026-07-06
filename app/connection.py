0bb4f4fcd15fb538843c838dbb72d9e14ec79456
MD Jahid Alam <alammdjahid081@gmail.com>
2026-07-06 12:51:41 +0530

    Prepare for Render deployment

--- app/connection.py
+++ app/connection.py
@@ -1,10 +1,32 @@
+import os
+
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker,declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:Jahid%40123@localhost/healthcare_db"
+# Load .env file
+load_dotenv()
 
-engine = create_engine(DATABASE_URL)
-  
+# Local ya Railway dono ke liye
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root:Jahid%40123@localhost/healthcare_db"
+)
+
+# Railway mysql:// ko SQLAlchemy format me convert karega
+if DATABASE_URL.startswith("mysql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "mysql://",
+        "mysql+pymysql://",
+        1
+    )
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
@@ -13,9 +35,12 @@ SessionLocal = sessionmaker(
 
 Base = declarative_base()
 
+
 def get_db():
     db = SessionLocal()
+
     try:
         yield db
+
     finally:
         db.close()
